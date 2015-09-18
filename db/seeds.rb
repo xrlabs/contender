fbb3530eab2750a2b66f4be58cb0cd2557140350
Robert Barisic <rbarisic@Roberts-MacBook-Pro.local>
2015-09-18 22:09:17 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,25 +19,75 @@ vitago = Space.create(name: 'ViTaGo')
 
 dolan.content_types.create([
     {
-        name: 'Article'
+        name: 'Article',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'title', type: 'text', regex: '/${abc}../i' },
+                { name: 'introduction', type: 'text', length: { minimum: 4, maximum: 8 } },
+                { name: 'content', type: 'text' }
+            ],
+            locals: [
+                { language: 'en' },
+                { language: 'de' },
+                { language: 'fr' }
+            ]
+        })
     },
     {
-        name: 'Judge'
+        name: 'Judge',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'name', type: 'text' },
+                { name: 'company', type: 'text' },
+                { name: 'occupation', type: 'text' }
+            ]
+        })
     },
     {
-        name: 'Mentor'
+        name: 'Mentor',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'name', type: 'text' },
+                { name: 'company', type: 'text' },
+                { name: 'occupation', type: 'text' }
+            ]
+        })
     }
 ])
 
 vitago.content_types.create([
     {
-        name: 'Personal'
+        name: 'Personal',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'name', type: 'text' },
+                { name: 'email', type: 'email' },
+                { name: 'password', type: 'password' }
+            ] 
+        })
     },
     {
-        name: 'Medikamente'
+        name: 'Medikamente',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'Medikament', type: 'text' },
+                { name: 'Art-Nr.', type: 'text' },
+                { name: 'Haltbarkeitsdatum', type: 'text' }
+            ],
+            locals: [
+                { language: 'de' }
+            ]
+        })
     },
     {
-        name: 'Räume'
+        name: 'Räume',
+        fields: ActiveSupport::JSON.encode({
+            fields: [
+                { name: 'number', type: 'text' },
+                { name: 'patient', type: 'text' },
+                { name: 'nurse', type: 'text' }
+            ]
+        })
     }
 ])
 
