796623b119384ec6eb02e5387cc663577b87a108
Robert Barisic <rbarisic@Roberts-MacBook-Pro.local>
2015-09-18 22:09:29 +0200

    small changes

--- app/helpers/entry_helper.rb
+++ app/helpers/entry_helper.rb
@@ -1,5 +1,6 @@
 module EntryHelper
 
+    # This methods turns the JSON strings into real HTML Input Elements
     # Expects a hash containing a Name and a Field Type, e.g. 'select' or 'text'
     def field_transform(name, field_type)
         case field_type
@@ -7,6 +8,10 @@ module EntryHelper
                 text_field_tag name
             when 'textbox'
                 text_area_tag name
+            when 'password'
+                password_field name
+            when 'email'
+                email_field name
             when 'select'
                 "not yet implemented"
             else
